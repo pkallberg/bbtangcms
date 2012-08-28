58f91c650b6a8899d314b5b332fad1e28094dad7
jhjguxin <864248765@qq.com>
2012-08-28 19:19:57 +0800

    fix error on news index page

--- config/deploy/vv.rb
+++ config/deploy/vv.rb
@@ -1,3 +1,3 @@
-set :repository,  "git@192.168.1.100:bbtang/bbtcms.git"
+set :repository,  "git@bbtang.com:bbtang/bbtcms.git"
 #set :repository,  "git@bbtang.com:bbtang/bbtcms.git"
 server "192.168.1.100", :app, :web, :db, :primary => true
