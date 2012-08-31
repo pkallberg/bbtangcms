0a672a47bee64c192c1fa332988908a11a118dae
jhjguxin <864248765@qq.com>
2012-08-31 11:30:46 +0800

    use spin.js to instead img background

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,8 +30,31 @@ function show() {
     $('#container').fadeIn();
 };
 
+function load_spin() {
+  var opts = {
+    lines: 17, // The number of lines to draw
+    length: 30, // The length of each line
+    width: 5, // The line thickness
+    radius: 20, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 11, // The rotation offset
+    color: '#B5B5B5', // #rgb or #rrggbb
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: true, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+  };
+  var target = document.getElementById('page_loading_process');
+  var spinner = new Spinner(opts).spin(target);
+}
 
-
+jQuery(document).ready(function($){
+load_spin()
+});
 
 
 $(document).ready(function(){
