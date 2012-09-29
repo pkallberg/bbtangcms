5335a67cea8109d17766889ffb9448d413691d13
Francis Jiang <864248765@qq.com>
2012-09-29 15:48:22 +0800

    try to unstand the task callback

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,10 +48,8 @@ set :deploy_to, "/home/#{user}/bbtang/#{application}"
 set :keep_releases, 15
 
 after 'deploy:update_code', 'deploy:migrate'
-after "deploy:symlink", "deploy:restart_workers"
-after "deploy:restart_workers","deploy:restart_crontab"
-after "deploy:restart_crontab", "rvm:trust_rvmrc"
-after "rvm:trust_rvmrc", "restart_workers"
+after "deploy:symlink", "deploy:restart_workers", "deploy:restart_crontab", "rvm:trust_rvmrc"
+
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
