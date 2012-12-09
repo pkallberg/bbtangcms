8151e3ffbb58594c5912da945bf5258022f07432
bbt_server <as181920@hotmail.com>
2012-12-09 21:02:58 +0800

    fix on production env cannot read data from 'Rails.configuration.self_settings'

--- config/initializers/write_config.rb
+++ config/initializers/write_config.rb
@@ -1,7 +1,22 @@
 # 将字典表、各配置写入Rails配置
+=begin
+Rails has 5 initialization events which can be hooked into (listed in the order that they are ran):
+* before_configuration: This is run as soon as the application constant inherits from Rails::Application. The config calls are evaluated before this happens.
+* before_initialize: This is run directly before the initialization process of the application occurs with the :bootstrap_hook initializer near the beginning of the Rails initialization process.
+* to_prepare: Run after the initializers are ran for all Railties (including the application itself), but before eager loading and the middleware stack is built. More importantly, will run upon every request in development, but only once (during boot-up) in production and test.
+* before_eager_load: This is run directly before eager loading occurs, which is the default behaviour for the production environment and not for the development environment.
+* after_initialize: Run directly after the initialization of the application, but before the application initializers are run
+=end
+#http://api.rubyonrails.org/classes/Rails/Railtie/Configuration.html
+# Not work on production
+#BBTangCMS::Application.config.after_initialize do
 
+# 这个写法太绕了,从数据库读取的数据去初始化 model的 一些验证参数，坑爹
+# production 环境会在应用加载的时候 初始化 model class(如下) 怎么办？？？？
+# 比如 这样: Creating scope :page. Overwriting existing method AdminSetting.page.
+# 最终在 prouduction 环境 初始化的时候 实际上 还是读取的数据库的数据 而不是 'BBTangCMS::Application.config.self_settings'
+# to_prepare 的时候 基于model的 hash 状态字段的多语言加载不完全的情况
 BBTangCMS::Application.config.after_initialize do
-
   BBTangCMS::Application.config.self_settings = {}
 
   ################ 字典表 #################
--- lib/askjane/meta_cache.rb
+++ lib/askjane/meta_cache.rb
@@ -20,45 +20,70 @@ module BBTangCMS
     end
 =end
     # 读取Rails字典表数据
+	#FIXME
+	#however on production Rails.configuration.respond_to?("self_settings") is false
+	#may be just because the write_config.rb require after the lib_core.rb #not work
+	#so mv write_config to lib/ folder and load after the meta_cache but #not work
+	# more info see #config/initializers/write_config.rb
+	# 最后 不管 Rails.configuration.self_settings 有没有 都去读取一下数据库以数据库为准
     def self.get_data_by_id(table_name, id)
-      if Rails.configuration.respond_to?("self_settings")
+      model_class = table_name.classify.constantize if table_name.class_exists?
+	  puts "get config data #{name} !!!!!!!!!#############################################################"  if Rails.env.development?
+      data = nil
+	  puts "is Rails.configuratio.self_settings present? #{Rails.configuration.respond_to?("self_settings")}"  if Rails.env.development?
+
+	  if Rails.configuration.respond_to?("self_settings")
         cache_value = Rails.configuration.self_settings[table_name + "_by_id"][id.to_s]
-        model_class = table_name.classify.constantize if table_name.class_exists?
-        if model_class.present?
-          if model_class.exists? :id => id
+        if cache_value.present?
+          data = cache_value
+        else	  
+          if model_class.present? and  model_class.exists? :id => id
             data = model_class.find id
             Rails.configuration.self_settings["#{table_name}_by_id"][id.to_s] = data
           end
-          data
-        else
-          data = cache_value
         end
       else
-        nil
+        if model_class.present? and  model_class.exists? :id => id
+          data = model_class.find id
+		  #Rails.configuration.self_settings["#{table_name}_by_id"][id.to_s] = data
+        end
       end
+	  puts "#{name} is #{data}"  if Rails.env.development?
+	  return data
     end
 
 
     def self.get_data_by_name(table_name, name)
-      if Rails.configuration.respond_to?("self_settings")
+      model_class = table_name.classify.constantize if table_name.class_exists?
+	  puts "get config data #{name} !!!!!!!!!#############################################################"  if Rails.env.development?
+      data = nil
+	  puts "is Rails.configuratio.self_settings present? #{Rails.configuration.respond_to?("self_settings")}"  if Rails.env.development?
+
+	  if Rails.configuration.respond_to?("self_settings")
         cache_value = Rails.configuration.self_settings[table_name + "_by_name"][name.to_s]
-        model_class = table_name.classify.constantize if table_name.class_exists?
-        if model_class.present?
+
+        if cache_value.present?
+		  data = cache_value
+        else
           if model_class.exists? :name => name
             data = model_class.find_by_name name
             Rails.configuration.self_settings["#{table_name}_by_name"][name.to_s] = data
           end
-          data
-        else
-          data = cache_value
         end
       else
-        nil
+        if model_class.exists? :name => name
+          data = model_class.find_by_name name
+          #Rails.configuration.self_settings["#{table_name}_by_name"][name.to_s] = data
+        end
       end
+	  puts "#{name} is #{data}"  if Rails.env.development?
+	  return data
     end
 
     def self.get_config_data(name)
+	  puts "get config data #{name} !!!!!!!!!#############################################################" if Rails.env.development?
       data = nil
+	  puts "is Rails.configuratio.self_settings present? #{Rails.configuration.respond_to?("self_settings")}"  if Rails.env.development?
       if Rails.configuration.respond_to?("self_settings")
         cache_value = Rails.configuration.self_settings[name]
         if cache_value.nil?
@@ -66,13 +91,17 @@ module BBTangCMS
             data = AdminSetting.find_by_name(name).value 
             Rails.configuration.self_settings[name] = data
           end
-          data
         else
           data = cache_value
         end
       else
-        nil
+        if AdminSetting.exists? :name => name
+          data = AdminSetting.find_by_name(name).value 
+		  #Rails.configuration.self_settings[name] = data
+        end
       end
+	  puts "#{name} is #{data}"  if Rails.env.development?
+	  return data
     end
 
     # 单个配置写入Rails设置（用于admin后台管理）
