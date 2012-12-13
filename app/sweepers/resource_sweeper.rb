bcf27bb14476d1a1bcd1ea47364c16aeea2691a1
jhjguxin <864248765@qq.com>
2012-12-13 17:16:55 +0800

    fix user conflict with observe and add 'route_checker?'

--- app/sweepers/resource_sweeper.rb
+++ app/sweepers/resource_sweeper.rb
@@ -13,11 +13,10 @@ The current_sign_in_at and last_sign_in_at are the two fields that are updated b
 =end
   # but not work for me so skip User model
   def after_update(resource)
-    expire_cache_for(resource)
-    #unless resource.current_sign_in_at_changed? or resource.last_sign_in_at_changed?
-    #unless resource.is_a User
-    #  expire_cache_for(resource)
-    #end
+    #expire_cache_for(resource)
+    unless resource.is_a? User and (resource.current_sign_in_at_changed? or resource.last_sign_in_at_changed?)
+      expire_cache_for(resource)
+    end
   end
  
   # If our sweeper detects that a Knowledge was deleted call this
@@ -35,18 +34,17 @@ The current_sign_in_at and last_sign_in_at are the two fields that are updated b
 =end
   private
   def expire_cache_for(resource)
-    #item = resource.is_a?(List) ? resource : resource.class
-    item = resource.class
     resource_name = resource.class.name.underscore.pluralize
 
     controller_name = is_namespace_resources(resource.class) ? "/#{resource_name}" : resource_name
     # Expire the index page now that we added a new resource
     #expire_page(:controller => resource_name, :action => 'index')
+
+    actions = %w( index show public feed ).collect{|action| action if route_checker?(controller_name,action)}.compact
+    expire_page(:controller => controller_name, :action => actions, :id => resource.id)
     
-    expire_page(:controller => controller_name, :action => %w( index show public feed ), :id => item.id)
-    
-    expire_action(:controller => controller_name, :action => "all")
-    item.shares.each { |share| expire_page(:controller => controller_name, :action => "show", :id => share.url_key) }
+    expire_action(:controller => controller_name, :action => actions)
+
     # Expire a fragment
     expire_fragment('all_available_'<< resource_name)
   end
@@ -65,5 +63,15 @@ The current_sign_in_at and last_sign_in_at are the two fields that are updated b
     observe_models = Setting.observe_models.split(" ")
     observe_models ||= ["Knowledge", "Note", "News", "Profile", "Question", "Quiz", "QuizCollection", "Subject", "User", "Answer", "Attachment", "CmsRole"]
     observe_models.collect{|object| object.constantize if object.class_exists?}.compact.uniq
+  end
+  def route_checker?(controller,action,opt={})
+    route_exist = true
+    begin
+      url_for({:controller => controller, :action => action}.merge opt)
+    rescue
+      # route not exist
+      route_exist = false
+    end
+    route_exist
   end
 end
