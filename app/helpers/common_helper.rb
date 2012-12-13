64d09691f9825adf9049e576aa06f85578fe7706
jhjguxin <864248765@qq.com>
2012-12-13 15:25:20 +0800

    fix one error on 'add_params_to_url'

--- app/helpers/common_helper.rb
+++ app/helpers/common_helper.rb
@@ -226,7 +226,8 @@ module CommonHelper
       uri = URI(url)             
       query_hash = Rack::Utils.parse_query(uri.query)
       query_hash.merge!(params)             
-      uri.query = Rack::Utils.build_query(query_hash)          
+      #uri.query = Rack::Utils.build_query(query_hash) #cannot use to nest_hash
+      uri.query = query_hash.to_param
       uri.to_s   
     end
   end
