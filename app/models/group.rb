7025e5f3bfcc050eb632c55d698b7d49e60cdd98
jhjguxin <864248765@qq.com>
2012-12-25 11:25:19 +0800

    try to make bbtcms work, after bbtang.com refactory, almost finished

--- app/models/group.rb
+++
@@ -1,6 +0,0 @@
-class Group < ActiveRecord::Base
-  #establish_connection :group_development
-  establish_connection "group_#{Rails.env}"
-
-  #include SphinxIndexable::Group
-end
--- app/models/knowledgebase_node.rb
+++
@@ -1,2 +0,0 @@
-class KnowledgebaseNode < ActiveRecord::Base
-end
